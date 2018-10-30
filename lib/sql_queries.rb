0d0dbc98ee95ab9aad49f0382cb159f512acf349
Justin Kenney <justinkenney13@gmail.com>
2018-10-30 21:14:02 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,7 +8,7 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 # "Write your SQL query Here"
-"SELECT projects.title, pledges.amount FROM projects
+"SELECT projects.title, SUM(pledges.amount) FROM projects
 JOIN pledges ON pledges.project_id = projects.id
 ORDER BY projects.title"
 end
