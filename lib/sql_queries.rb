368695786c2fe1e07b0de3dddeae56965c115781
Justin Kenney <justinkenney13@gmail.com>
2018-10-30 21:20:57 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -16,6 +16,7 @@ end
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
 "SELECT users.*, pledges.amount FROM users
 JOIN pledges ON pledges.user_id = users.id
+GROUP BY users.name
 ORDER by users.name"
 end
 
