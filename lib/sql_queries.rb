52e08739c5df33b11628768bc8a58d6fb3c6fd4d
Justin Kenney <justinkenney13@gmail.com>
2018-10-30 21:04:41 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,6 +8,8 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "Write your SQL query Here"
+SELECT projects.title FROM projects
+JOIN pledges ON pledges.project_id = projects.id
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
