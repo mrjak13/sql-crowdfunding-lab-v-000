57cd8510f587b692bb1727e9e4119457ebc5e7f6
Justin Kenney <justinkenney13@gmail.com>
2018-10-30 20:57:23 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -6,3 +6,16 @@ CREATE TABLE projects (
   start_date INTEGER,
   end_date INTEGER
 );
+
+CREATE TABLE user (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER  
+);
+
+CREATE TABLE pledge (
+  id INTEGER PRIMARY KEY;
+  amount INTEGER,
+  user_id INTEGER,
+  project_id INTEGER
+);
